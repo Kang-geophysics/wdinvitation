f41deeb10020e83be59d207bd16f56fe1e5a3577
hyeonwoo <hykanghw@gmail.com>
2026-02-27 00:22:01 +0900

    v6

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -3,12 +3,28 @@ import ImageGallery from "react-image-gallery";
 import { Divider } from "antd";
 import styled from "styled-components";
 
-import GalleryPhoto1 from "../assets/Gallery_Photo_1.webp";
-import GalleryPhoto2 from "../assets/Gallery_Photo_2.webp";
-import GalleryPhoto3 from "../assets/Gallery_Photo_3.webp";
-import GalleryPhoto4 from "../assets/Gallery_Photo_4.webp";
-import GalleryPhoto5 from "../assets/Gallery_Photo_5.webp";
-import GalleryPhoto6 from "../assets/Gallery_Photo_6.webp";
+// import GalleryPhoto1 from "../assets/Gallery_Photo_1.webp";
+// import GalleryPhoto2 from "../assets/Gallery_Photo_2.webp";
+// import GalleryPhoto3 from "../assets/Gallery_Photo_3.webp";
+// import GalleryPhoto4 from "../assets/Gallery_Photo_4.webp";
+// import GalleryPhoto5 from "../assets/Gallery_Photo_5.webp";
+// import GalleryPhoto6 from "../assets/Gallery_Photo_6.webp";
+
+import GalleryPhoto1 from "../assets/GalleryPhoto_1.jpg";
+import GalleryPhoto2 from "../assets/GalleryPhoto_2.jpg";
+import GalleryPhoto3 from "../assets/GalleryPhoto_3.jpg";
+import GalleryPhoto4 from "../assets/GalleryPhoto_4.jpg";
+import GalleryPhoto5 from "../assets/GalleryPhoto_5.jpg";
+import GalleryPhoto6 from "../assets/GalleryPhoto_6.jpg";
+import GalleryPhoto7 from "../assets/GalleryPhoto_7.jpg";
+import GalleryPhoto8 from "../assets/GalleryPhoto_8.jpg";
+import GalleryPhoto9 from "../assets/GalleryPhoto_9.jpg";
+import GalleryPhoto10 from "../assets/GalleryPhoto_10.jpg";
+import GalleryPhoto11 from "../assets/GalleryPhoto_11.jpg";
+import GalleryPhoto12 from "../assets/GalleryPhoto_12.jpg";
+import GalleryPhoto13 from "../assets/GalleryPhoto_13.jpg";
+import GalleryPhoto14 from "../assets/GalleryPhoto_14.jpg";
+import GalleryPhoto15 from "../assets/GalleryPhoto_15.jpg";
 
 const Wrapper = styled.div`
   padding-top: 42px;
@@ -49,6 +65,42 @@ const images = [
   {
     original: GalleryPhoto6,
     thumbnail: GalleryPhoto6,
+  },
+  {
+    original: GalleryPhoto7,
+    thumbnail: GalleryPhoto7,
+  },
+  {
+    original: GalleryPhoto8,
+    thumbnail: GalleryPhoto8,
+  },
+  {
+    original: GalleryPhoto9,
+    thumbnail: GalleryPhoto9,
+  },
+  {
+    original: GalleryPhoto10,
+    thumbnail: GalleryPhoto10,
+  },
+  {
+    original: GalleryPhoto11,
+    thumbnail: GalleryPhoto11,
+  },
+  {
+    original: GalleryPhoto12,
+    thumbnail: GalleryPhoto12,
+  },
+  {
+    original: GalleryPhoto13,
+    thumbnail: GalleryPhoto13,
+  },
+  {
+    original: GalleryPhoto14,
+    thumbnail: GalleryPhoto14,
+  },
+  {
+    original: GalleryPhoto15,
+    thumbnail: GalleryPhoto15,
   },
 ];
 
