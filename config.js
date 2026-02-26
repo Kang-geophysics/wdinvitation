ba0c4e76b2107daece4d9aaac7daeba1aa8493d0
hyeonwoo <hykanghw@gmail.com>
2026-02-26 23:40:38 +0900

    v2

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "2026년 03월 01일, 일요일 오전 13시 00분";
+export const WEDDING_DATE = "2026년 03월 01일, 일요일 오전 12시 00분";
 export const WEDDING_LOCATION = "LOTTE HOTELS & RESRORTS MOMOYAMA";
 
 export const GROOM_NAME = "강현우";
