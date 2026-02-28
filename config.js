f830219e24f9fb7d3c1bf9ec46f1d1ed7c8f9bd1
hyeonwoo <hykanghw@gmail.com>
2026-02-28 21:29:28 +0900

    final
    url

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-export const WEDDING_INVITATION_URL = "http://localhost:8000/";
+export const WEDDING_INVITATION_URL = "https://wdinvitationkangandkim.vercel.app/";
 export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "2026년 03월 01일 (일) 오전 12시 00분";
+export const WEDDING_DATE = "2026년 03월 01일 (일) 오후 12시 00분";
 export const WEDDING_LOCATION = "LOTTE HOTEL & MOMOYAMA";
 
 export const GROOM_NAME = "강현우";
